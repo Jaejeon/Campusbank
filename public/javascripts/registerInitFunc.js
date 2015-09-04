2bf5ffbe068be9558e7aca6e9cb9a1516ca2cc07
Jaejeon <jaejeon9@gmail.com>
2015-09-04 18:53:34 +0900

    MODIFYING register.jade

---
+++ public/javascripts/registerInitFunc.js
@@ -0,0 +1,15 @@
+
+//if there is ajax-post link, should be there .js file having 'addPostLink' function
+var addPostLink = function(){
+    //TODO
+    //post link handler append
+};
+
+var pageInit = function(){
+
+    //TODO
+    //page init function
+    addPostLink();
+    $('#regist-form').validator();
+};
+
