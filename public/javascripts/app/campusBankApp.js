6232e1128d63d480656ad46c0935c590b4f9cd88
variel <blackmagic2@naver.com>
2015-11-13 14:42:27 +0900

    투자부 페이지들 추가.

---
+++ public/javascripts/app/campusBankApp.js
@@ -0,0 +1,37 @@
+﻿(function() {
+    var app
+        = angular
+            .module('campusBankApp', ['ngRoute']);
+
+    app.config(['$routeProvider', routeConfig]);
+    app.config(['$locationProvider', locationConfig]);
+
+    function routeConfig($provider) {
+        $provider
+            .when('/invest', {
+                templateUrl: '/templates/invest/introduce.html'
+            }).when('/invest/products', {
+                templateUrl: '/templates/invest/product-list.html',
+                controller: 'investProductListController',
+                controllerAs: 'productList'
+            }).when('/invest/products/:productId', {
+                templateUrl: '/templates/invest/product-detail.html',
+                controller: 'investProductDetailController',
+                controllerAs: 'productDetail'
+            }).when('/invest/cart', {
+                templateUrl: '/templates/invest/cart.html',
+                controller: 'investCartController',
+                controllerAs: 'cart'
+            }).when('/invest/requests', {
+                templateUrl: '/templates/invest/request-list.html',
+                controller: 'investRequestListController',
+                controllerAs: 'requestList'
+            });
+    }
+
+    function locationConfig($provider) {
+        $provider
+            .html5Mode(true)
+            .hashPrefix('!');
+    }
+})();
